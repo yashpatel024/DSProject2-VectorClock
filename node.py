cbe7af64afeb0f219ecf09a6dd0cf5676d075247
Blindman139 <parth395006vpj@gmai.com>
2022-07-10 15:30:35 +0530

    added help section and message sections

--- node.py
+++ node.py
@@ -3,29 +3,35 @@ import time
 import json
 import threading
 from sys import argv
-from sockets import send_request, increament_clock, create_server, internal_process
+from sockets import print_help, send_request, increament_clock, create_server, internal_process
 
 
 def sender(clock_val, self_id, port_list):
     while True:
-        node_input = input().strip()
+        input_line = input().strip()  # ID Message
+        # Split into parts
+        inputs = input_line.split(" ", 1)
 
-        if node_input in ["e", "exit"]:
+        id = inputs[0].strip().lower()
+        message = inputs[1] if len(inputs) > 1 else ""
+        if id in ["e", "exit"]:
             break
+        elif id in ["h", "help"]:
+            print_help(len(port_list))
+            continue
 
         # Not valid values
         # Not numeric, not int, out of range
-
         invalid_flag = False
-        if node_input.isnumeric():
-            send_id = int(node_input, 10)
+        if id.isnumeric():
+            send_id = int(id, 10)
             if send_id <= len(port_list):
                 print(f"Before: {clock_val}")
                 if send_id == self_id:
                     increament_clock(clock_val, self_id)
-                    internal_process(self_id)
+                    internal_process(self_id, message)
                 else:
-                    send_request(self_id, port_list[send_id-1], increament_clock(clock_val, self_id))
+                    send_request(self_id, port_list[send_id-1], increament_clock(clock_val, self_id), message)
                 print(f"After: {clock_val}\n")
             else:
                 invalid_flag = True
@@ -51,7 +57,7 @@ if __name__ == "__main__":
 
     clock_val = [0]*node_count
 
-    t1 = threading.Thread(target=create_server, args=[self_id, port_list[self_id-1], clock_val])
+    t1 = threading.Thread(target=create_server, args=[self_id, port_list, clock_val])
     t1.start()
 
     sender(clock_val, self_id, port_list)
--- sockets.py
+++ sockets.py
@@ -1,15 +1,26 @@
-import math
-from sys import argv, exit, stderr
 from socket import socket, AF_INET, SOCK_STREAM
-from select import select as sel
-from pathlib import Path
 import json
 
 IP = "127.0.0.1"
 
 
-def internal_process(self_id):
-    print(f"Internal process completed of {self_id}")
+def print_help(total_client):
+    width = 60
+    print()
+    print("~"*width)
+    print(f"{'HELP':^{width}}")
+    print("~"*width)
+    print(
+        "# To get help anytime enter \"help\"\n"
+        "# Message Format: [{TO_CLIENT_ID} {MESSAGE_TO_SEND}]\n"
+        f"# Possible range of values of TO_CLIENT_ID: [1-{total_client}]"
+    )
+    print("~"*width, end="\n\n")
+
+
+def internal_process(self_id, message):
+    print(f"Internal process completed of {self_id} with message:")
+    print(message)
 
 
 def increament_clock(clock_val, id):
@@ -23,26 +34,38 @@ def compare_clock(self_clock, sender_clock):
     return self_clock
 
 
-def create_server(self_id, self_port, clock_val):
+def create_server(self_id, port_list, clock_val):
     with socket(AF_INET, SOCK_STREAM) as sock:
-        sock.bind((IP, self_port))
+        sock.bind((IP, port_list[self_id-1]))
         sock.listen(10)
-        print(f"[+] Listening on localhost at: {(IP, self_port)}")
+        print(f"[+] Listening at: {(IP, port_list[self_id-1])}")
+        print_help(len(port_list))
         while True:
             conn, addr = sock.accept()
             req = json.loads(conn.recv(65536))
-            print(f"Received from Node {req.get('sender_id')}")
+            print(f"Message received from Node {req.get('sender_id')}:")
+            print(req.get("message", ""))
+
             print(f"Before = {clock_val}")
             increament_clock(clock_val, self_id)
-            print(f"After = {compare_clock(clock_val, req.get('data'))} \n")
+
+            print(f"After = {compare_clock(clock_val, req.get('sender_clock'))} \n")
             conn.send(json.dumps({"a": "success"}).encode())
             conn.close()
 
 
-def send_request(sender_id, port, data):
-    json_data = {"data": data, "sender_id": sender_id}
+def send_request(sender_id, port, clock_val, message):
+    json_data = {
+        "sender_clock": clock_val,
+        "sender_id": sender_id,
+        "message": message
+    }
     sock = socket(AF_INET, SOCK_STREAM)
     sock.connect((IP, port))
     sock.send(json.dumps(json_data).encode())
     response = json.loads(sock.recv(65536))
     sock.close()
+
+
+if __name__ == "__main__":
+    print_help(5)
